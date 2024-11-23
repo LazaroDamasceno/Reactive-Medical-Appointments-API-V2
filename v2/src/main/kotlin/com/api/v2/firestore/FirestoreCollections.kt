83b18fd1bee65192b63b865b3440a7c3ad1ef5fa
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-23 01:34:41 -0300

    Created the class FirestoreCollections

---
+++ v2/src/main/kotlin/com/api/v2/firestore/FirestoreCollections.kt
@@ -0,0 +1,16 @@
+package com.api.v2.firestore
+
+import com.google.cloud.firestore.CollectionReference
+import com.google.firebase.cloud.FirestoreClient
+
+class FirestoreCollections {
+
+    companion object {
+
+        fun getPeopleCollectionReference(): CollectionReference {
+            return FirestoreClient.getFirestore().collection("people")
+        }
+
+    }
+
+}
