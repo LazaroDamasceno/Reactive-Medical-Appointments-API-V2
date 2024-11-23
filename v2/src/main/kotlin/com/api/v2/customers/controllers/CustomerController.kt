280f262c3df6effde1513977dc58e30fefb09f24
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-23 14:37:12 -0300

    Added one missing RequestBody annotation to CustomerController

--- v2/src/main/kotlin/com/api/v2/customers/controllers/CustomerController.kt
+++ v2/src/main/kotlin/com/api/v2/customers/controllers/CustomerController.kt
@@ -3,18 +3,15 @@ package com.api.v2.customers.controllers
 import com.api.v2.customers.domain.Customer
 import com.api.v2.customers.dtos.CustomerRegistrationDto
 import com.api.v2.customers.services.CustomerRegistrationService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RestController
 @RequestMapping("api/v2/customers")
 class CustomerController(
     private val registrationService: CustomerRegistrationService
 ) {
 
     @PostMapping
-    suspend fun register(registrationDto: CustomerRegistrationDto): Customer {
+    suspend fun register(@RequestBody registrationDto: CustomerRegistrationDto): Customer {
         return registrationService.register(registrationDto)
     }
 
