6760727d0e67a803c9dfbdb74ca324e93eb8c189
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-23 13:45:17 -0300

    Turned the no-args constructors og Customer and Person into primary constructors

--- v2/src/main/kotlin/com/api/v2/customers/domain/Customer.kt
+++ v2/src/main/kotlin/com/api/v2/customers/domain/Customer.kt
@@ -2,23 +2,26 @@ package com.api.v2.customers.domain
 
 import com.api.v2.customers.dtos.CustomerAddressDto
 import com.api.v2.people.domain.Person
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 import java.time.ZoneId
 
-class Customer {
+@Document
+class Customer(
+    var person: Person,
+    var address: CustomerAddressDto
+) {
 
-    var id: String = ""
-    lateinit var person: Person
-    lateinit var address: CustomerAddressDto
+    @BsonId
+    val id: ObjectId = ObjectId()
     val createdAt: String = LocalDateTime.now().toString()
     val createdAtZone: String = ZoneId.systemDefault().toString()
 
     companion object {
         fun create(person: Person, addressDto: CustomerAddressDto): Customer {
-            val customer = Customer()
-            customer.person = person
-            customer.address = addressDto
-            return customer
+            return Customer(person, addressDto)
         }
     }
 
--- v2/src/main/kotlin/com/api/v2/people/domain/Person.kt
+++ v2/src/main/kotlin/com/api/v2/people/domain/Person.kt
@@ -4,30 +4,34 @@ import com.api.v2.people.dtos.PersonFullNameDto
 import com.api.v2.people.dtos.PersonRegistrationDto
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 
-class Person {
+@Document
+class Person(
+    var fullName: PersonFullNameDto,
+    var ssn: String,
+    var birthDate: LocalDate,
+    var email: String,
+    var phoneNumber: String
+) {
 
     @BsonId
     val id: ObjectId = ObjectId()
-    lateinit var fullName: PersonFullNameDto
-    var ssn: String = ""
-    var birthDate: String = ""
-    var email: String = ""
-    var phoneNumber: String = ""
-    val createdAt: String = LocalDateTime.now().toString()
-    val createdAtZone: String = ZoneId.systemDefault().toString()
+    val createdAt: LocalDateTime = LocalDateTime.now()
+    val createdAtZone: ZoneId = ZoneId.systemDefault()
 
     companion object {
         fun create(registrationDto: PersonRegistrationDto): Person {
-            val person = Person()
-            person.fullName = registrationDto.fullNameDto
-            person.ssn = registrationDto.ssn
-            person.birthDate = registrationDto.birthDate.toString()
-            person.email = registrationDto.email
-            person.phoneNumber = registrationDto.phoneNumber
-            return person
+            return Person(
+                registrationDto.fullNameDto,
+                registrationDto.ssn,
+                registrationDto.birthDate,
+                registrationDto.email,
+                registrationDto.phoneNumber
+            )
         }
     }
 
