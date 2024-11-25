e74e5cb2e96c04e49ba2fbf2936aef89d22f5ca5
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-25 20:21:28 -0300

    Removed the unused import from CustomerController

--- v2/src/main/kotlin/com/api/v2/customers/controllers/CustomerController.kt
+++ v2/src/main/kotlin/com/api/v2/customers/controllers/CustomerController.kt
@@ -1,6 +1,5 @@
 package com.api.v2.customers.controllers
 
-import com.api.v2.customers.domain.Customer
 import com.api.v2.customers.dtos.CustomerModificationDto
 import com.api.v2.customers.dtos.CustomerRegistrationDto
 import com.api.v2.customers.dtos.CustomerResponseDto
