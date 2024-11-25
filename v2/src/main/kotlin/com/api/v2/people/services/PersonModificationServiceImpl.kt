de0a4516c3cc56017c07454cd4d87adc40aae48e
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-25 15:57:54 -0300

    Modified PersonModificationServiceImpl

--- v2/src/main/kotlin/com/api/v2/people/services/PersonModificationServiceImpl.kt
+++ v2/src/main/kotlin/com/api/v2/people/services/PersonModificationServiceImpl.kt
@@ -1,6 +1,8 @@
 package com.api.v2.people.services
 
 import com.api.v2.people.domain.Person
+import com.api.v2.people.domain.PersonAuditTrail
+import com.api.v2.people.domain.PersonAuditTrailRepository
 import com.api.v2.people.domain.PersonRepository
 import com.api.v2.people.dtos.PersonModificationDto
 import jakarta.validation.Valid
@@ -11,10 +13,13 @@ import org.springframework.stereotype.Service
 
 @Service
 internal class PersonModificationServiceImpl(
-    private val personRepository: PersonRepository
+    private val personRepository: PersonRepository,
+    private val personAuditTrailRepository: PersonAuditTrailRepository
 ): PersonModificationService {
     override suspend fun modify(person: @NotNull Person, modificationDto: @Valid PersonModificationDto): Person {
         return withContext(Dispatchers.IO) {
+            val personAuditTrail = PersonAuditTrail.create(person)
+            personAuditTrailRepository.save(personAuditTrail)
             person.modify(modificationDto)
             personRepository.save(person)
         }
