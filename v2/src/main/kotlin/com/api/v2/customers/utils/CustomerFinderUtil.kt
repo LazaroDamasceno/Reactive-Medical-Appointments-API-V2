4e4ba6cdde46761a2c830c4fb84f7bef0e281107
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-25 14:15:21 -0300

    Created the util CustomerFinderUtil

---
+++ v2/src/main/kotlin/com/api/v2/customers/utils/CustomerFinderUtil.kt
@@ -0,0 +1,30 @@
+package com.api.v2.customers.utils
+
+import com.api.v2.customers.domain.Customer
+import com.api.v2.customers.domain.CustomerRepository
+import com.api.v2.customers.exceptions.CustomerNotFoundException
+import com.api.v2.people.utils.PersonFinderUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.withContext
+import org.springframework.stereotype.Component
+
+@Component
+class CustomerFinderUtil(
+    private val personFinderUtil: PersonFinderUtil,
+    private val customerRepository: CustomerRepository
+) {
+
+    suspend fun find(ssn: String): Customer {
+        return withContext(Dispatchers.IO) {
+            val foundPerson = personFinderUtil.find(ssn)
+            if (foundPerson == null) throw CustomerNotFoundException()
+            customerRepository
+                .findAll()
+                .filter { c -> c.person == foundPerson }
+                .single()
+        }
+    }
+
+}
