470a5e18c72e56db85a547107f27aa52537615d2
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-26 13:41:13 -0300

    Created the controller DoctorController

---
+++ v2/src/main/kotlin/com/api/v2/people/controllers/DoctorController.kt
@@ -0,0 +1,23 @@
+package com.api.v2.people.controllers
+
+import com.api.v2.doctors.dtos.DoctorRegistrationDto
+import com.api.v2.doctors.dtos.DoctorResponseDto
+import com.api.v2.doctors.services.DoctorRegistrationService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/doctors")
+class DoctorController(
+    private val registrationService: DoctorRegistrationService
+) {
+
+    @PostMapping
+    suspend fun register(@RequestBody registrationDto: @Valid DoctorRegistrationDto): DoctorResponseDto {
+        return registrationService.register(registrationDto)
+    }
+
+}
