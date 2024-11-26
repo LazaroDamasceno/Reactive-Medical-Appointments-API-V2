e23f1f5fa1a2871fdc316f09d6b2f5b3c1a7a772
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-26 18:43:07 -0300

    cHANGED DoctorRetrievalServiceImpl

--- v2/src/main/kotlin/com/api/v2/doctors/controllers/DoctorController.kt
+++ v2/src/main/kotlin/com/api/v2/doctors/controllers/DoctorController.kt
@@ -7,6 +7,7 @@ import com.api.v2.doctors.services.DoctorRegistrationService
 import com.api.v2.doctors.services.DoctorRetrievalService
 import jakarta.validation.Valid
 import kotlinx.coroutines.flow.Flow
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -17,11 +18,13 @@ class DoctorController(
 ) {
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     suspend fun register(@RequestBody registrationDto: @Valid DoctorRegistrationDto): DoctorResponseDto {
         return registrationService.register(registrationDto)
     }
 
     @GetMapping("by-medical-license-number")
+    @ResponseStatus(value = HttpStatus.OK)
     suspend fun findByMedicalLicenseNumber(
         @RequestBody licenseNumberDto: @Valid MedicalLicenseNumberDto
     ): DoctorResponseDto {
@@ -29,6 +32,7 @@ class DoctorController(
     }
 
     @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
     suspend fun findAll(): Flow<DoctorResponseDto> {
         return retrievalService.findAll()
     }
--- v2/src/main/kotlin/com/api/v2/doctors/services/DoctorRetrievalServiceImpl.kt
+++ v2/src/main/kotlin/com/api/v2/doctors/services/DoctorRetrievalServiceImpl.kt
@@ -5,6 +5,7 @@ import com.api.v2.doctors.dtos.DoctorResponseDto
 import com.api.v2.doctors.dtos.MedicalLicenseNumberDto
 import com.api.v2.doctors.utils.DoctorFinderUtil
 import com.api.v2.doctors.utils.DoctorResponseMapper
+import jakarta.validation.Valid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -17,7 +18,9 @@ internal class DoctorRetrievalServiceImpl(
     private val doctorRepository: DoctorRepository
 ): DoctorRetrievalService {
 
-    override suspend fun findByMedicalLicenseNumber(licenseNumberDto: MedicalLicenseNumberDto): DoctorResponseDto {
+    override suspend fun findByMedicalLicenseNumber(
+        licenseNumberDto: @Valid MedicalLicenseNumberDto
+    ): DoctorResponseDto {
         return withContext(Dispatchers.IO) {
             val foundDoctor = doctorFinderUtil.findByMedicalLicenseNumber(licenseNumberDto)
             DoctorResponseMapper.map(foundDoctor)
@@ -26,9 +29,7 @@ internal class DoctorRetrievalServiceImpl(
 
     override suspend fun findAll(): Flow<DoctorResponseDto> {
         return withContext(Dispatchers.IO) {
-            doctorRepository
-                .findAll()
-                .map { d -> DoctorResponseMapper.map(d) }
+            doctorRepository.findAll().map { d -> DoctorResponseMapper.map(d) }
         }
     }
 
